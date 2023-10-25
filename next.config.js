0e32357cd08ad59d8605958c3fadc41dd378ef32
Thais <thaisdsilve@gmail.com>
2023-10-24 23:36:14 -0300

    add new domain to nextjs config image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+    ],
   },
 };
 
