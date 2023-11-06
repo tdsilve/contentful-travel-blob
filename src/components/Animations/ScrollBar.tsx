a835809970a4d29a979cf879fda4f1313d71f787
Thais <thaisdsilve@gmail.com>
2023-11-06 00:45:07 -0300

    remove console.log

--- src/components/Animations/ScrollBar.tsx
+++ src/components/Animations/ScrollBar.tsx
@@ -4,9 +4,6 @@ import { useScroll, motion } from "framer-motion";
 import React from "react";
 export const ScrollBar = () => {
   const { scrollYProgress } = useScroll();
-  React.useEffect(() => {
-    console.log(scrollYProgress);
-  }, [scrollYProgress]);
   return (
     <motion.div
       style={{
